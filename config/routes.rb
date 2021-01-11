b9c95669c3a1bcaeac64fba7caabd44d3a0b7083
Cory Finger <cory.j.finger@gmail.com>
2021-01-10 20:46:30 -0800

    Fix CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*all', to: proc { [204, {}, ['']] }, via: :options
   get "/health_check" => "application#health_check"
   post "assertions/failures" => "assertions#failure_create"
 end
