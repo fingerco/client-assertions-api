0b1d7d433346ca2c114e5b7a7fb92e260f7ac8be
Cory Finger <cory.j.finger@gmail.com>
2021-01-10 19:19:21 -0800

    Bump version

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 210574254273.dkr.ecr.us-west-2.amazonaws.com && \
 docker build -t client-assertions-api:$1 . && \
 docker tag client-assertions-api:$1 210574254273.dkr.ecr.us-west-2.amazonaws.com/client-assertions-api:$1 && \
-docker push 210574254273.dkr.ecr.us-west-2.amazonaws.com/client-assertions-api:$1
+docker push 210574254273.dkr.ecr.us-west-2.amazonaws.com/client-assertions-api:$1 && \
+echo "Latest Built Version: $1" > version.txt
